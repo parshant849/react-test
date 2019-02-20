19d52752e53ee291407a01752241e1f17ca79a74
Parshant <parshant@ucreate.co.in>
2019-02-20 12:02:13 +0530

    first commit

---
+++ precache-manifest.094015a69e80b9d37990bd44839eee49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "34d8a4a4506afc735e93",
+    "url": "/static/js/main.34d8a4a4.chunk.js"
+  },
+  {
+    "revision": "b85ee129ea6bf0db48f0",
+    "url": "/static/js/1.b85ee129.chunk.js"
+  },
+  {
+    "revision": "34d8a4a4506afc735e93",
+    "url": "/static/css/main.6106d494.chunk.css"
+  },
+  {
+    "revision": "9aedf3e9f4e271733eab90f710532f4d",
+    "url": "/index.html"
+  }
+];
