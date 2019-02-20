559eaa1d938d92b82ff055610cd92171147ae719
Parshant <parshant@ucreate.co.in>
2019-02-20 15:11:18 +0530

    sdfasdfsdaf

--- index.js
+++ index.js
@@ -5,14 +5,22 @@ var app = express()
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', function(request, response) {
-  response.writeHead(302,  {Location: "https://www.abcdefghijkl.ml"})
-  // response.end();
-  // if(request.headers['x-forwarded-proto']!=='https'){
-  //   request.redirect(301, 'https://www.abcdefghijkl.ml'+request.url);
-  // }  
-  response.send('Hello World!')
-})
+// app.get('/', function(request, response) {
+//   response.writeHead(302,  {Location: "https://www.abcdefghijkl.ml"})
+//   // response.end();
+//   // if(request.headers['x-forwarded-proto']!=='https'){
+//   //   request.redirect(301, 'https://www.abcdefghijkl.ml'+request.url);
+//   // }  
+//   response.send('Hello World!')
+// })
+
+app.get('*', function(req, res, next) {
+  if (req.headers.host.slice(0, 3) != 'www') {
+    res.redirect('http://www.' + req.headers.host + req.url, 301);
+  } else {
+    response.send('Hello World!');
+  }
+});
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
